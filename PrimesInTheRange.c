388e2a1445ed306d129cf51d2e5af67eaabe6f73
chsrkh <chsrk@gmail.com>
2025-07-18 23:12:33 +0530

    Primes in the range

---
+++ PrimesInTheRange.c
@@ -0,0 +1,35 @@
+//c program to print all primes in the range
+#include <stdio.h>
+int isPrime(int num) {
+    if (num <= 1) return 0; // 0 and 1 are not prime numbers
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return 0; // Not a prime number
+    }
+    return 1; // Is a prime number
+}
+int main() {
+    int start, end;
+
+    // Input the range
+    printf("Enter the start of the range: ");
+    scanf("%d", &start);
+    printf("Enter the end of the range: ");
+    scanf("%d", &end);
+
+    // Validate the range
+    if (start > end) {
+        printf("Invalid range. Start should be less than or equal to end.\n");
+        return 1;
+    }
+
+    // Print prime numbers in the range
+    printf("Prime numbers in the range [%d, %d] are:\n", start, end);
+    for (int i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
